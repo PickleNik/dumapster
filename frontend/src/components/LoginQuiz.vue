f4f3ef881b30c77664e17ca9bd7a1dabb6d2db6a
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-04-08 01:30:28 -0400

    vue login quiz component

---
+++ frontend/src/components/LoginQuiz.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div class="bg-emerald-950 m-8 rounded-3xl p-24">
+    <h1 class="text-4xl font-black text-emerald-400">LoginQuiz</h1>
+  </div>
+</template>
+
+<style scoped></style>
