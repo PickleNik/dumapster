d553f801b5fd376f45f8aeeb21801136e8800131
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-04-08 15:05:24 -0400

    preferred classes

---
+++ frontend/src/components/classList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="border border-neutral-600 rounded-xl m-2 bg-neutral-900 p-4"
+    v-for="course in $classes"
+  >
+    {{ course.name }}
+  </div>
+</template>
+
+<script setup>
+import { classes } from "../classStore";
+import { useStore } from "@nanostores/vue";
+
+// read the store value with the `useStore` hook
+const $classes = useStore(classes);
+</script>
--- frontend/src/components/search.vue
+++ frontend/src/components/search.vue
@@ -7,30 +7,51 @@
       v-model="query"
     />
     <div
+      v-if="query"
       class="flex absolute right-4 top-18 max-h-[80vh] overflow-y-auto flex-col bg-neutral-900 rounded-xl gap-2 overflow-hidden border border-neutral-400"
     >
-      <div
-        v-for="course in courses"
-        class="bg-neutral-800 text-neutral-300 rounded-lg p-3"
+      <button
+        v-for="course in courses.filter((c) => c.type == 'course')"
+        class="bg-neutral-800 text-neutral-300 rounded-lg p-3 cursor-pointer hover:bg-neutral-700 active:bg-neutral-600"
+        @click="addClass(course)"
       >
         {{ course.name }}
-      </div>
+      </button>
     </div>
   </div>
 </template>
 
 <script setup>
+import { classes } from "../classStore";
+import { useStore } from "@nanostores/vue";
+
+// read the store value with the `useStore` hook
+const $classes = useStore(classes);
+
 import { ref, watch } from "vue";
 let query = ref("");
 let courses = ref([]);
 
+function addClass(course) {
+  let dup = false;
+  for (let i in $classes.value) {
+    if ({ ...$classes.value[i] }.name === course.name) {
+      dup = true;
+      return;
+    }
+  }
+
+  if (!dup) classes.set([...$classes.value, course]);
+}
+
 // call api on query change
 watch(query, async () => {
-  console.log(query.value);
   const response = await fetch(
-    `https://planetterp.com/api/v1/search?limit=100&query=${query.value}`
+    `https://planetterp.com/api/v1/search?limit=100&query=${query.value.replace(
+      /\s/g,
+      ""
+    )}`
   );
   courses = await response.json();
-  console.log(courses);
 });
 </script>
