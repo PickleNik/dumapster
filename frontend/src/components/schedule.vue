f8e555bddcc9fe223624da1a9b8e99b78765dc9e
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-04-08 10:24:03 -0400

    schedule page & dark mode

---
+++ frontend/src/components/schedule.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex">
+    <div class="flex rounded-3xl overflow-hidden gap-2 flex-wrap max-w-96">
+      <div v-for="d in ['m', 't', 'w', 'r', 'f']" class="flex flex-col gap-2">
+        <div
+          class="hover:bg-neutral-700 duration-300 cursor-pointer w-48 h-24 rounded-xl bg-gray-200 dark:bg-neutral-800"
+        ></div>
+        <div
+          class="hover:bg-neutral-700 duration-150 cursor-pointer w-48 h-24 rounded-xl bg-gray-200 dark:bg-neutral-800"
+        ></div>
+        <div
+          class="hover:bg-neutral-700 duration-150 cursor-pointer w-48 h-24 rounded-xl bg-gray-200 dark:bg-neutral-800"
+        ></div>
+        <div
+          class="hover:bg-neutral-700 duration-150 cursor-pointer w-48 h-24 rounded-xl bg-gray-200 dark:bg-neutral-800"
+        ></div>
+        <div
+          class="hover:bg-neutral-700 duration-150 cursor-pointer w-48 h-24 rounded-xl bg-gray-200 dark:bg-neutral-800"
+        ></div>
+        <div
+          class="hover:bg-neutral-700 duration-150 cursor-pointer w-48 h-24 rounded-xl bg-gray-200 dark:bg-neutral-800"
+        ></div>
+        <div
+          class="hover:bg-neutral-700 duration-150 cursor-pointer w-48 h-24 rounded-xl bg-gray-200 dark:bg-neutral-800"
+        ></div>
+        <div
+          class="hover:bg-neutral-700 duration-150 cursor-pointer w-48 h-24 rounded-xl bg-gray-200 dark:bg-neutral-800"
+        ></div>
+        <div
+          class="hover:bg-neutral-700 duration-150 cursor-pointer w-48 h-24 rounded-xl bg-gray-200 dark:bg-neutral-800"
+        ></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
---
+++ frontend/src/components/search.vue
@@ -0,0 +1,9 @@
+<template>
+  <input
+    type="text"
+    placeholder="Search"
+    class="px-3 py-2 dark:bg-neutral-800 dark:text-white text-lg rounded-2xl m-4 focus:outline-0 focus:ring-1 ring-neutral-400"
+  />
+</template>
+
+<script setup></script>
