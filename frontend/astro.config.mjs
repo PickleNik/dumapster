fcd7ad4591ef85a411ea544858e3a78cd584fbe0
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-04-08 19:02:42 -0400

    vercel stuff

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,10 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import vue from "@astrojs/vue";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), vue()]
+  integrations: [tailwind(), react(), vue()],
+  output: "server",
+  adapter: vercel({
+    analytics: true,
+  }),
 });
