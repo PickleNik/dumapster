6d1ca3111cfc027a4be394f5c4005ea10656ba5d
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-04-08 22:03:59 -0400

    new prisma tet

--- frontend/api/handler.js
+++ frontend/api/handler.js
@@ -1,6 +1,6 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+// import { PrismaClient } from "@prisma/client";
+import prisma from "../lib/prisma";
+// const prisma = new PrismaClient();
 
 async function main() {
   // ... you will write your Prisma Client queries here
@@ -23,6 +23,8 @@ export const config = {
 export default async (request) => {
   // const prisma = new PrismaClient();
   // use `prisma` in your application to read and write data in your DB
-  // const users = await prisma.curr_classes2.findMany();
-  return new Response(`Hello, from ${request.url} I'm now an Edge Function!`);
+  const users = await prisma.curr_classes2.findMany();
+  return new Response(
+    `Hello, from ${request.url} I'm now an Edge Function!${users[0]}`
+  );
 };
