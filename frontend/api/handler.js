750e8aa594f7fb8de5ea0ee5cc998361071f6552
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-04-08 21:46:00 -0400

    prisma test

--- frontend/api/handler.js
+++ frontend/api/handler.js
@@ -1,8 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
+
 export const config = {
   runtime: "edge",
 };
-
-// import { PrismaClient } from "@prisma/client";
 
 export default async (request) => {
   // const prisma = new PrismaClient();
