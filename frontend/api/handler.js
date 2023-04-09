aa600fc66ee95b1356fe091d6e4bb1cc96e71a25
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-04-08 21:42:57 -0400

    undo

--- frontend/api/handler.js
+++ frontend/api/handler.js
@@ -8,7 +8,5 @@ export default async (request) => {
   const prisma = new PrismaClient();
   // use `prisma` in your application to read and write data in your DB
   const users = await prisma.curr_classes2.findMany();
-  return new Response(
-    `Hello, from ${request.url} I'm now an Edge Function!${users}`
-  );
+  return new Response(`Hello, from ${request.url} I'm now an Edge Function!`);
 };
