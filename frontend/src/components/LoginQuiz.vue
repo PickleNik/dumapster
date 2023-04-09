8a9cc867a80bb6e5505c08a059f60ee36cadfa91
Daniel Fesenko <dfesenko@terpmail.umd.edu>
2023-04-09 01:54:41 -0400

    updated login

--- frontend/src/components/LoginQuiz.vue
+++ frontend/src/components/LoginQuiz.vue
@@ -1,9 +1,33 @@
-<script setup></script>
+
+<script setup>
+const majors = await fetch('https://api.umd.io/v0/majors');
+    const majorsJson = await majors.json();
+    console.log(majorsJson);
+
+    const majorsList = majorsJson.map((major) => {
+      return major.major_name;
+    });
+
+    const isCommuter = false;
+
+</script>
 
 <template>
-  <div class="bg-emerald-950 m-8 rounded-3xl p-24">
-    <h1 class="text-4xl font-black text-emerald-400">LoginQuiz</h1>
+  
+  <div class="bg-gray-800 m-8 rounded-3xl p-24 justify-items-center">
+    <div class="flex flex-col items-center">
+    <h2 class="text-3xl font-black text-white text-center">What is your major?</h2>
+
+</div>
+    <div class="flex flex-col items-center">
+      <h2 class="text-3xl font-black text-white text-center">Are you a commuter?</h2>
+      <div class="flex justify-center space-x-3">
+        <button class="px-4 group relative flex justify-center rounded-md bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Yes</button>
+        <button class="px-5 group relative flex justify-center rounded-md bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">No</button>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
--- frontend/src/components/ReactLogin.jsx
+++ frontend/src/components/ReactLogin.jsx
@@ -28,42 +28,61 @@ export const ReactLogin = () => {
 
 
   return (
-    <div>
+    <div className="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
+      <div className="w-full max-w-md space-y-8">
       {currentForm === "login" ? (
         <div>
-          <form id="loginform">
-            <label htmlFor="email">Email</label>
-            <input onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email" id="email" name="email" />
-            <br></br>
-            <label htmlFor="password">Password</label>
-            <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" id="password" name="password" />
-            <button onClick={handleLogin}>Log in</button>
-            <p>Don't have an account?</p>
-            <button onClick={(e) => {
+          <h2 class="mt-6 text-center text-3xl font-extrabold text-white">Log in</h2>
+          <form id="loginform" class="mt-8 space-y-6">
+            <div class="-space-y-px rounded-md shadow-sm">
+            <label htmlFor="email" class="sr-only">Email</label>
+            <input onChange={(e) => setEmail(e.target.value)} type="email" placeholder=" Email" id="email" name="email" required class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <label htmlFor="password" class="sr-only">Password</label>
+            <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder=" Password" id="password" name="password" required class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+            <div class="flex items-center justify-between">
+            <button onClick={handleLogin} class="group relative flex w-full justify-center rounded-md bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Log in</button>
+            </div>
+            </div>
+            <div class="flex items-center justify-center">
+            <p>Don't have an account? </p>
+            <a href="" class="font-medium text-indigo-600 hover:text-indigo-500 ml-1" onClick={(e) => {
             e.preventDefault();
             setCurrentForm("register");
-            }}>Register</button>
+            }}> Register</a>
+            </div>
           </form>
         </div>
       ) : (
         <div>
-          <form id="registerform">
-            <label htmlFor="name">Name</label>
-            <input onChange={(e) => setName(e.target.value)} type="name" placeholder="Full Name" id="name" name="name" />
-            <br></br>
-            <label htmlFor="registeremail">Email</label>
-            <input onChange={(e) => setRegisterEmail(e.target.value)} type="email" placeholder="Email" id="registeremail" name="registeremail" />
-            <label htmlFor="registerpassword">Password</label>
-            <input onChange={(e) => setRegisterPassword(e.target.value)} type="password" placeholder="Password" id="registerpassword" name="registerpassword" />
-            <button onClick={handleRegister}>Register</button>
+          <h1 class="mt-6 text-center text-3xl font-extrabold text-white">Create Account</h1>
+          <form id="registerform" class="mt-8 space-y-1">
+            <div class="-space-y-px rounded-md shadow-sm">
+            <label htmlFor="name" class="sr-only">Name</label>
+            <input onChange={(e) => setName(e.target.value)} type="name" placeholder=" Full Name" id="name" name="name" required class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+            <label htmlFor="registeremail" class="sr-only">Email</label>
+            <input onChange={(e) => setRegisterEmail(e.target.value)} type="email" placeholder=" Email" id="registeremail" name="registeremail" required class="relative block w-full border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+            <label htmlFor="registerpassword" class="sr-only">Password</label>
+            <input onChange={(e) => setRegisterPassword(e.target.value)} type="password" placeholder=" Password" id="registerpassword" name="registerpassword" required class="relative block w-full rounded-b-md mb-80 border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+            <div class="flex items-center justify-between">
+            <button onClick={handleRegister} class="group relative flex w-full justify-center rounded-md bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            ><span class="absolute inset-y-0 left-0 flex items-center pl-3">
+            <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+              <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
+            </svg>
+          </span>Register</button>
+          </div>
+            <div class="flex items-center justify-center">
             <p>Already have an account?</p>
-            <button onClick={(e) => {
-              e.preventDefault();
-              setCurrentForm("login");
-              }}>Log in</button>
+            <a href="" class="font-medium text-indigo-600 hover:text-indigo-500 ml-1" onClick={(e) => {
+            e.preventDefault();
+            setCurrentForm("login");
+            }}>Log in</a>
+            </div>
+            </div>
           </form>
         </div>
       )}
+      </div>
     </div>
   )
 }
