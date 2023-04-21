3e7cbffd8bd988585b8cc252e35c300b0efa4f6b
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-04-20 22:58:50 -0400

    fix build

--- frontend/src/components/LoginQuiz.vue
+++ frontend/src/components/LoginQuiz.vue
@@ -1,20 +1,19 @@
 <script setup>
-import { ref } from "vue";
-import { ref, watch } from 'vue'
+import { ref, watch } from "vue";
 
 let dropDown = ref(false);
-let filter = ref('');
+let filter = ref("");
 watch(filter, async () => {
-console.log
+  console.log;
 });
 
-const majors = await fetch('https://api.umd.io/v0/majors');
-    const majorsJson = await majors.json();
-    console.log(majorsJson);
+const majors = await fetch("https://api.umd.io/v0/majors");
+const majorsJson = await majors.json();
+console.log(majorsJson);
 
-    const majorsList = majorsJson.map((major) => {
-      return major.major_name;
-    });
+const majorsList = majorsJson.map((major) => {
+  return major.major_name;
+});
 
 const isCommuter = false;
 </script>
@@ -22,28 +21,29 @@ const isCommuter = false;
 <template>
   <div class="bg-gray-800 m-8 rounded-3xl p-24 justify-items-center">
     <div class="flex flex-col items-center">
-    <h2 class="text-3xl font-black text-white text-center">What is your major?</h2>
-    <div class="relative">
-      <input atclick="dropDown = !dropDown">
-      <div v-if = "dropDown" class="absolute top-8 bg-white w-full">
-        test
-      </div>
-    </div>
-    <div class="flex flex-col items-center">
       <h2 class="text-3xl font-black text-white text-center">
-        Are you a commuter?
+        What is your major?
       </h2>
-      <div class="flex justify-center space-x-3">
-        <button
-          class="px-4 group relative flex justify-center rounded-md bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Yes
-        </button>
-        <button
-          class="px-5 group relative flex justify-center rounded-md bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          No
-        </button>
+      <div class="relative">
+        <input atclick="dropDown = !dropDown" />
+        <div v-if="dropDown" class="absolute top-8 bg-white w-full">test</div>
+      </div>
+      <div class="flex flex-col items-center">
+        <h2 class="text-3xl font-black text-white text-center">
+          Are you a commuter?
+        </h2>
+        <div class="flex justify-center space-x-3">
+          <button
+            class="px-4 group relative flex justify-center rounded-md bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Yes
+          </button>
+          <button
+            class="px-5 group relative flex justify-center rounded-md bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            No
+          </button>
+        </div>
       </div>
     </div>
   </div>
